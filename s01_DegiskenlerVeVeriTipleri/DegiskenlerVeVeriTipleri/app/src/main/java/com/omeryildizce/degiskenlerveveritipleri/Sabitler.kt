77b0f9bb66def9bf210a902b3b259188247b3b09
omeryildizce <omeryildizce@gmail.com>
2023-02-13 23:10:42 +0300

    Create Sabitler.kt

---
+++ s01_DegiskenlerVeVeriTipleri/DegiskenlerVeVeriTipleri/app/src/main/java/com/omeryildizce/degiskenlerveveritipleri/Sabitler.kt
@@ -0,0 +1,13 @@
+package com.omeryildizce.degiskenlerveveritipleri
+
+fun main(args: Array<String>) {
+    var sayi = 10
+    sayi = 20
+    sayi = 40
+    println(sayi)
+
+    val sayi1 = 40
+    val pi = 3.1415
+    val klorOrani = 4.5
+    val isim = "ahmet"
+}
