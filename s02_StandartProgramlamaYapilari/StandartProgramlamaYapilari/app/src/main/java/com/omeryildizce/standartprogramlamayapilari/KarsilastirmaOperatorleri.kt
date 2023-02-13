b2ee5d7b46a406734260c82fb47beb6cae90456a
omeryildizce <omeryildizce@gmail.com>
2023-02-13 23:20:52 +0300

    Create KarsilastirmaOperatorleri.kt

---
+++ s02_StandartProgramlamaYapilari/StandartProgramlamaYapilari/app/src/main/java/com/omeryildizce/standartprogramlamayapilari/KarsilastirmaOperatorleri.kt
@@ -0,0 +1,21 @@
+package com.omeryildizce.standartprogramlamayapilari
+
+fun main() {
+    var s1 = 60
+    var s2 = 50
+
+    var v1 = 70
+    var v2 = 80
+
+    println(s1 == s2)
+    println(s1 != s2)
+    println(s1 > s2)
+    println(s1 >= s2)
+    println(s1 < s2)
+    println(s1 <= s2)
+
+    println(s1>s2 || v1>v2)
+    println(s1>s2 && v1>v2)
+
+
+}
