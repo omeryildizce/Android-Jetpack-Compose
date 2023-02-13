9629cc80835077e9a679ffe134f786b574983cba
omeryildizce <omeryildizce@gmail.com>
2023-02-13 23:20:50 +0300

    Create IfCalismasi.kt

---
+++ s02_StandartProgramlamaYapilari/StandartProgramlamaYapilari/app/src/main/java/com/omeryildizce/standartprogramlamayapilari/IfCalismasi.kt
@@ -0,0 +1,19 @@
+package com.omeryildizce.standartprogramlamayapilari
+
+fun main() {
+    var yas = 19
+    var isim = "mehmet"
+
+    if (yas >= 18){
+        println("Bilet alabilir")
+    }
+    else{
+        println("Bilet alamaz")
+    }
+    if (isim =="ahmet"){
+        println("ürünü alabilir")
+    }else{
+        println("ürünü alamaz")
+    }
+
+}
