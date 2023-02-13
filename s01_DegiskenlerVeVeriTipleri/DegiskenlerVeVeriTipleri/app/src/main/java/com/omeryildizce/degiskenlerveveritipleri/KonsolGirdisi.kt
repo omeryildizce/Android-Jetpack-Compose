67af36d6778e4f345227cfbf9efb0c2a05b0f6c0
omeryildizce <omeryildizce@gmail.com>
2023-02-13 23:10:40 +0300

    Create KonsolGirdisi.kt

---
+++ s01_DegiskenlerVeVeriTipleri/DegiskenlerVeVeriTipleri/app/src/main/java/com/omeryildizce/degiskenlerveveritipleri/KonsolGirdisi.kt
@@ -0,0 +1,10 @@
+package com.omeryildizce.degiskenlerveveritipleri
+
+fun main(args: Array<String>) {
+    println("Adınızı giriniz:")
+    var ad = readLine()
+    println(ad)
+
+    val a = readLine()?.toInt()
+    println(a)
+}
