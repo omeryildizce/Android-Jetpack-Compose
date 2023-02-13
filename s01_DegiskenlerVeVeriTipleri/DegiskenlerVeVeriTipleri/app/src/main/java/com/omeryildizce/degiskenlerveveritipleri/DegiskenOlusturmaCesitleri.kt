4dad04fa6ffbce8bf5507b97b367a094539c7914
omeryildizce <omeryildizce@gmail.com>
2023-02-13 23:10:33 +0300

    Create DegiskenOlusturmaCesitleri.kt

---
+++ s01_DegiskenlerVeVeriTipleri/DegiskenlerVeVeriTipleri/app/src/main/java/com/omeryildizce/degiskenlerveveritipleri/DegiskenOlusturmaCesitleri.kt
@@ -0,0 +1,19 @@
+package com.omeryildizce.degiskenlerveveritipleri
+
+fun main(args: Array<String>) {
+    var sayi = 10
+    sayi = 20
+
+    var fiyat = 12.99
+    println(fiyat)
+    fiyat = 15.99
+    println(fiyat)
+
+    var s1 = 50
+    var s2 = 54
+    var toplam = s1 + s2
+    println(toplam)
+
+    var sonuc = 100
+    sonuc = 10.5.toInt()
+}
