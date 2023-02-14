f442c18cdde5137044238a0abcdab7d4d2f52853
omeryildizce <omeryildizce@gmail.com>
2023-02-14 17:08:39 +0300

    Create BreakContinue.kt

---
+++ s02_StandartProgramlamaYapilari/StandartProgramlamaYapilari/app/src/main/java/com/omeryildizce/standartprogramlamayapilari/BreakContinue.kt
@@ -0,0 +1,15 @@
+package com.omeryildizce.standartprogramlamayapilari
+
+fun main() {
+    for (i in 1..5){
+        if (i ==3)
+            break
+        println("Döngü 1: $i")
+    }
+
+    for (i in 1..5){
+        if (i ==3)
+            continue
+        println("Döngü 1: $i")
+    }
+}
