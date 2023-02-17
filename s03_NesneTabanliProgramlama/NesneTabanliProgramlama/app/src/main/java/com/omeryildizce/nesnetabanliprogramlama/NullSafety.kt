3ffbf3a868396883951274135ba437d848f618f2
omeryildizce <omeryildizce@gmail.com>
2023-02-17 19:33:42 +0300

    Create NullSafety.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/NullSafety.kt
@@ -0,0 +1,9 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+fun main() {
+    var isim: String? = null
+    println(isim?.trim())
+    if (isim != null){
+        println(isim.trim())
+    }
+}
