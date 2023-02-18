51bbf200c2a0b62f26a388791960be47ac8ab732
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:53:33 +0300

    Create HesaplayiciMain.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/HesaplayiciMain.kt
@@ -0,0 +1,11 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+fun main() {
+
+    val hesaplayici:Hesaplayici = Hesaplayici()
+
+    hesaplayici.topla(10, 10)
+    hesaplayici.topla(10, 10.5)
+    hesaplayici.topla(10.5, 10)
+    hesaplayici.topla(10.5, 10.5)
+}
