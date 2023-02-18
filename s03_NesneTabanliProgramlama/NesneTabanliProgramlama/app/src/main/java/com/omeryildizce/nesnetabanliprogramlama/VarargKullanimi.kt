11ce13844cc7c8a74ed09ab2c695c219ed2c0535
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:51:03 +0300

    Create VarargKullanimi.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/VarargKullanimi.kt
@@ -0,0 +1,12 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+fun main() {
+
+    toplam(1,2,3,3,2,2,2,25,5,6,6,5,4,)
+}
+
+fun toplam(vararg sayilar:Int) {
+
+    println(sayilar.sum())
+
+}
