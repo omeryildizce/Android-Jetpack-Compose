bd08b3fc2c46ff57d3c3aac900916ad94c5917f6
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:53:43 +0300

    Create Odev1Main.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/Odev1Main.kt
@@ -0,0 +1,10 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+fun main() {
+
+    var derece1 = Odev1()
+
+    var fahrenayt1 = derece1.fahrenayt(50.0)
+    println(fahrenayt1)
+    println(derece1.derece(fahrenayt1))
+}
