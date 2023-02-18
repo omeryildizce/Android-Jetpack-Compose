867e3e6630c2ef255374e70f1d80385568019651
omeryildizce <omeryildizce@gmail.com>
2023-02-18 19:44:42 +0300

    Create InterfaceOrnekMain.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/InterfaceOrnekMain.kt
@@ -0,0 +1,19 @@
+package com.omeryildizce.nesnetabanliprogramlama2
+
+fun main() {
+    val aslan = Aslan()
+    val amasyaElması:Elma = AmasyaElması()
+    val elma = Elma()
+    val tavuk:Eatable = Tavuk()
+
+    val nesneler = arrayOf(aslan,amasyaElması,elma,tavuk)
+    for (nesne in nesneler){
+        if (nesne is Eatable){
+            nesne.howToEat()
+        }
+        if (nesne is Squeezable){
+            nesne.howToSqueeze()
+        }
+
+    }
+}
