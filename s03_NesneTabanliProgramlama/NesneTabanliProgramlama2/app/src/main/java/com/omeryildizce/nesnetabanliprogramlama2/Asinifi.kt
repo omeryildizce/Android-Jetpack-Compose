41e11d618738890a645925d9d406e676f0338c2c
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:51:29 +0300

    Create Asinifi.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Asinifi.kt
@@ -0,0 +1,10 @@
+package com.omeryildizce.nesnetabanliprogramlama2
+
+class Asinifi {
+    companion object{
+        var x = 10
+        fun metod(){
+            println("Merhaba")
+        }
+    }
+}
