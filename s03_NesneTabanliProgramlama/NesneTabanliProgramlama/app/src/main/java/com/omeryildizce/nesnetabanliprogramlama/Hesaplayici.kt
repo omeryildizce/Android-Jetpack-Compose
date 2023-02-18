fd305a16621db9b4e356effba8cf3ec2e0e8a75b
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:50:43 +0300

    Create Hesaplayici.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/Hesaplayici.kt
@@ -0,0 +1,17 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+class Hesaplayici {
+    fun topla(sayi1:Int, sayi2:Int){
+        println(sayi1+sayi2)
+    }
+    fun topla(sayi1:Int, sayi2:Double){
+        println(sayi1+sayi2)
+    }
+    fun topla(sayi1:Double, sayi2:Int){
+        println(sayi1+sayi2)
+    }
+    fun topla(sayi1:Double, sayi2:Double){
+        println(sayi1+sayi2)
+    }
+
+}
