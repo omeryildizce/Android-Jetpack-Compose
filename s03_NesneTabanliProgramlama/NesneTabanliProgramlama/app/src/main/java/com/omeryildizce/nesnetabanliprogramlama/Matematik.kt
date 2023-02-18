42b6875a32a00d6c3049754bef5e5076f1a27d65
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:53:37 +0300

    Create Matematik.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/Matematik.kt
@@ -0,0 +1,13 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+class Matematik {
+    fun topla(sayi1:Int, sayi2:Int){
+        val toplam = sayi1 + sayi2
+        println("Toplam: $toplam")
+    }
+
+    fun cikarma(sayi1:Int, sayi2:Int){
+        val fark = sayi1 + sayi2
+        println("Fark: $fark")
+    }
+}
