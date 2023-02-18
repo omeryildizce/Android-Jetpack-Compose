b0cdf1a5dc5e3b079e9f69f0ee48c45828725a8e
omeryildizce <omeryildizce@gmail.com>
2023-02-18 16:33:52 +0300

    Update Araba.kt

--- s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Araba.kt
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Araba.kt
@@ -2,4 +2,7 @@ package com.omeryildizce.nesnetabanliprogramlama2
 
 open class Araba(var kasaTipi: String, renk: String, vites: String) : Arac(renk, vites) {
 
+    override fun toString(): String {
+        return  super.toString() +" ,kasaTipi='$kasaTipi'"
+    }
 }
