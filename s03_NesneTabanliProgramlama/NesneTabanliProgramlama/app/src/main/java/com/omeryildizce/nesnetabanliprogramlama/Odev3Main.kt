500a4dace3dca4e9e69dae39c6301baa9bb12967
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:50:46 +0300

    Create Odev3Main.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/Odev3Main.kt
@@ -0,0 +1,8 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+fun main() {
+    val sayi = 5
+    val f = Odev3()
+    var faltoriyel =  f.faktoriyelHesaplama(sayi)
+    println(faltoriyel)
+}
