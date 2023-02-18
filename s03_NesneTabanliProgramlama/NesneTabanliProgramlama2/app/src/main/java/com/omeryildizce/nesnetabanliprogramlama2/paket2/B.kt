4ab66773b99f2d0295ff74999d625dbae3ef86fb
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:51:59 +0300

    Create B.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/paket2/B.kt
@@ -0,0 +1,12 @@
+package com.omeryildizce.nesnetabanliprogramlama2.paket2
+
+import com.omeryildizce.nesnetabanliprogramlama2.paket1.A
+
+open class B  {
+    var varsayilanDegisken = 1
+    public var publicDegisken = 2
+    private var privateDegisken = 3
+    internal var internalDegisken = 4
+    protected var protectedDegisken = 5
+
+}
