1750f602a22ac73dee7dd6243ef05b1d9194869e
omeryildizce <omeryildizce@gmail.com>
2023-02-18 19:44:47 +0300

    Create ClassA.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/ClassA.kt
@@ -0,0 +1,14 @@
+package com.omeryildizce.nesnetabanliprogramlama2
+
+class ClassA : Interface1 {
+    override val degisken: Int = 10
+
+    override fun metod1() {
+        println("interface")
+    }
+
+    override fun metod2(): String {
+        return "interface"
+
+    }
+}
