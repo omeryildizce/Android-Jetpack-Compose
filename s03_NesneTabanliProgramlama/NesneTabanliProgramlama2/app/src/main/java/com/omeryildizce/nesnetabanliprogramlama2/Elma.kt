3c428a53e0605af73c716b1574090b8c8b47b084
omeryildizce <omeryildizce@gmail.com>
2023-02-18 19:44:52 +0300

    Create Elma.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Elma.kt
@@ -0,0 +1,12 @@
+package com.omeryildizce.nesnetabanliprogramlama2
+
+open class Elma:Squeezable,Eatable {
+    override fun howToEat() {
+        println("Dilimle ve ye")
+    }
+
+    override fun howToSqueeze() {
+        println("Blender ile sık")
+    }
+
+}
