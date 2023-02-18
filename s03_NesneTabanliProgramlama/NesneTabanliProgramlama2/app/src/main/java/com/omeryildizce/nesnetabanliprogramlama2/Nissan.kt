8f18f0710094db5ad6471b034e43ea0cad247db8
omeryildizce <omeryildizce@gmail.com>
2023-02-18 16:34:17 +0300

    Update Nissan.kt

--- s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Nissan.kt
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Nissan.kt
@@ -1,5 +1,7 @@
 package com.omeryildizce.nesnetabanliprogramlama2
 
 class Nissan(var model: String, vites: String, renk: String, kasaTipi: String) : Araba(kasaTipi,renk,vites){
-
+    override fun toString(): String {
+        return super.toString() + ", model='$model'"
+    }
 }
