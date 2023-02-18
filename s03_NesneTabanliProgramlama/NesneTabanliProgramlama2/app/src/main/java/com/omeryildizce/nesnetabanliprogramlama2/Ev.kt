558bacaadf64003a109c13bb91844295a184b0e1
omeryildizce <omeryildizce@gmail.com>
2023-02-18 16:34:00 +0300

    Create Ev.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Ev.kt
@@ -0,0 +1,8 @@
+package com.omeryildizce.nesnetabanliprogramlama2
+
+open class Ev(var pencereSayisi:Int) {
+
+    override fun toString(): String {
+        return "pencereSayisi=$pencereSayisi"
+    }
+}
