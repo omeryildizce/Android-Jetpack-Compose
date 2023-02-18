745d926971157fed8f7061e926d0035a8e068446
omeryildizce <omeryildizce@gmail.com>
2023-02-18 16:34:05 +0300

    Update KalitimMain.kt

--- s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/KalitimMain.kt
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/KalitimMain.kt
@@ -2,4 +2,8 @@ package com.omeryildizce.nesnetabanliprogramlama2
 
 fun main() {
     val araba = Araba("sedan","beyaz", "otomatik")
+    println(araba.toString())
+
+    val nissan = Nissan("micra","manuel","beyaz","sedan")
+    println(nissan.toString())
 }
