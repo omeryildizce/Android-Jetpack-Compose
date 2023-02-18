95db74ba1a761f5302a2042a14e8772e2a1b2897
omeryildizce <omeryildizce@gmail.com>
2023-02-19 02:08:46 +0300

    Create ArrayListIslemleri.kt

---
+++ s04_Collections/Collections/app/src/main/java/com/omeryildizce/collections/ArrayListIslemleri.kt
@@ -0,0 +1,30 @@
+package com.omeryildizce.collections
+
+fun main() {
+    val meyveler =  ArrayList<String>()
+    meyveler.add("Çilek")
+    meyveler.add("Muz")
+    meyveler.add("Elma")
+    meyveler.add("Kivi")
+    meyveler.add("Kiraz")
+    meyveler.add("Karpuz")
+    println(meyveler.isEmpty())
+    println(meyveler.count())
+    println(meyveler.size)
+    println(meyveler.first())
+    println(meyveler.last())
+    println(meyveler.indices)
+    println(meyveler.contains("Muz"))
+    println(meyveler.max())
+    println(meyveler.min())
+    meyveler
+    println(meyveler.toString())
+    meyveler.reverse()
+    println(meyveler.toString())
+    meyveler.removeAt(3)
+    println(meyveler.toString())
+    meyveler.remove("Muz")
+    println(meyveler.toString())
+    meyveler.clear()
+    println(meyveler.toString())
+}
