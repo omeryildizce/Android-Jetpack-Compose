1f5275d5aaef062c6f504e95f94476656d5db0c7
omeryildizce <omeryildizce@gmail.com>
2023-02-18 16:33:58 +0300

    Update Arac.kt

--- s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Arac.kt
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama2/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama2/Arac.kt
@@ -1,5 +1,7 @@
 package com.omeryildizce.nesnetabanliprogramlama2
 
 open class Arac(var renk:String, var vites:String){
-
+    override fun toString(): String {
+        return "renk='$renk', vites='$vites'"
+    }
 }
