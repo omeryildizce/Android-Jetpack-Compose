8251bd03a7ef9417a3c70d7bdae7e1c00feeaae5
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:50:50 +0300

    Create Odev5.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/Odev5.kt
@@ -0,0 +1,11 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+class Odev5 {
+    fun icAciToplami(kenarSayisi:Int):Int{
+        if (kenarSayisi>2){
+            return (kenarSayisi - 2) * 180
+        }
+        else return 0
+
+    }
+}
