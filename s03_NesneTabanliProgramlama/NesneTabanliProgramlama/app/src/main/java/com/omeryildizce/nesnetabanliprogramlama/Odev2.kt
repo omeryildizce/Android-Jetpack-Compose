9efe244bd96564bc0994ded6b47f68d1427f0163
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:53:45 +0300

    Create Odev2.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/Odev2.kt
@@ -0,0 +1,7 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+class Odev2 {
+    fun dikdortgenCevresi(en: Int, boy: Int) {
+        println("Didörtgenin çevresi: ${(en + boy) * 2} br")
+    }
+}
