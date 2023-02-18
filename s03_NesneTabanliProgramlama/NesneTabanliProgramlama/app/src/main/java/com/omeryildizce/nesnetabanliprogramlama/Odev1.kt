5f7ac58b790b5c7c653fbed22c7dbacecf02faa4
omeryildizce <omeryildizce@gmail.com>
2023-02-18 15:51:11 +0300

    Create Odev1.kt

---
+++ s03_NesneTabanliProgramlama/NesneTabanliProgramlama/app/src/main/java/com/omeryildizce/nesnetabanliprogramlama/Odev1.kt
@@ -0,0 +1,11 @@
+package com.omeryildizce.nesnetabanliprogramlama
+
+class Odev1 {
+    fun fahrenayt(derece:Double) : Double{
+        return derece * 1.8 + 32
+    }
+    fun derece(fahrenayt:Double) : Double{
+        return (fahrenayt - 32) / 1.8
+    }
+
+}
