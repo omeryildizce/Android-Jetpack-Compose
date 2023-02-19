62a1ced7d792d5d4a7079ebc32b07e904fd70005
omeryildizce <omeryildizce@gmail.com>
2023-02-19 18:22:05 +0300

    Update Ogrenci.kt

--- s04_Collections/Collections/app/src/main/java/com/omeryildizce/collections/Ogrenci.kt
+++ s04_Collections/Collections/app/src/main/java/com/omeryildizce/collections/Ogrenci.kt
@@ -1,7 +1,18 @@
 package com.omeryildizce.collections
 
-class Ogrenci(var no: Int, var ad:String, var sinif:String) {
+class Ogrenci(var no: Int, var ad: String, var sinif: String) {
+    override fun hashCode(): Int {
+        return this.no
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this.no == (other as Ogrenci).no) {
+            return true
+        }
+        return false
+    }
+
     override fun toString(): String {
-        return "Ogrenci(no=$no, ad='$ad', sinif='$sinif')"
+        return String.format("Ogrenci no: %4d, Adı: %-15s, Sınıf: %3s", no, ad, sinif)
     }
 }
