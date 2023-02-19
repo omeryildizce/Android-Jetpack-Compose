923c8687d70e7c6830283345914796a52d8c8f49
omeryildizce <omeryildizce@gmail.com>
2023-02-19 15:15:08 +0300

    Create Ogrenci.kt

---
+++ s04_Collections/Collections/app/src/main/java/com/omeryildizce/collections/Ogrenci.kt
@@ -0,0 +1,7 @@
+package com.omeryildizce.collections
+
+class Ogrenci(var no: Int, var ad:String, var sinif:String) {
+    override fun toString(): String {
+        return "Ogrenci(no=$no, ad='$ad', sinif='$sinif')"
+    }
+}
