53f7c4304661a40aa80008169bea4f77ddbc348f
omeryildizce <omeryildizce@gmail.com>
2023-02-22 22:28:03 +0300

    Update MainActivity.kt

--- s10_JetpackComposeWidgets/SayiTahminUygulamasi/app/src/main/java/com/omeryildizce/sayitahminuygulamasi/MainActivity.kt
+++ s10_JetpackComposeWidgets/SayiTahminUygulamasi/app/src/main/java/com/omeryildizce/sayitahminuygulamasi/MainActivity.kt
@@ -3,13 +3,29 @@ package com.omeryildizce.sayitahminuygulamasi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.omeryildizce.sayitahminuygulamasi.ui.theme.SayiTahminUygulamasiTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    AnaSayfa()
+                    SayfaGecisleri()
                 }
             }
         }
@@ -30,13 +46,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AnaSayfa() {
+fun SayfaGecisleri() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "anasayfa") {
+        composable("anasayfa") {
+            AnaSayfa(navController = navController)
+        }
+        composable("tahmin_ekrani") {
+            TahminEkrani(navController = navController)
+        }
+        composable("sonuc_ekrani/{sonuc}", arguments = listOf(
+            navArgument("sonuc"){type = NavType.BoolType}
+        )) {
+            val sonuc = it.arguments?.getBoolean("sonuc") ?: false
+            SonucEkrani(sonuc = sonuc)
+        }
+    }
+}
+
+@Composable
+fun AnaSayfa(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Tahmin Oyunu", fontSize = 36.sp)
+        Image(
+            painter = painterResource(id = R.drawable.zar_resim),
+            contentDescription = "zar resmi"
+        )
+        Button(
+            onClick = {
+                navController.navigate("tahmin_ekrani")
+            },
+            modifier = Modifier.size(250.dp, 50.dp)
+        ) {
+            Text(text = "Oyuna Başla")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SayiTahminUygulamasiTheme {
-        AnaSayfa()
+        SayfaGecisleri()
     }
 }
