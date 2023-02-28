ff1761d99d24ab1f45e53096dd3cb5f899ddeb2b
omeryildizce <omeryildizce@gmail.com>
2023-02-28 22:16:50 +0300

    Update Kisiler.kt

--- s14_DepolamaIslemleri/KisilerUygulamasi/app/src/main/java/com/omeryildizce/kisileruygulamasi/entity/Kisiler.kt
+++ s14_DepolamaIslemleri/KisilerUygulamasi/app/src/main/java/com/omeryildizce/kisileruygulamasi/entity/Kisiler.kt
@@ -1,3 +1,14 @@
 package com.omeryildizce.kisileruygulamasi.entity
 
-data class Kisiler(var kisi_id:Int, val kisi_ad:String, var kisi_tel :String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity("kisiler")
+data class Kisiler(
+    @PrimaryKey(true)
+    @ColumnInfo(name = "kisi_id") @NotNull var kisi_id: Int,
+    @ColumnInfo(name = "kisi_ad") @NotNull val kisi_ad: String,
+    @ColumnInfo(name = "kisi_tel") @NotNull var kisi_tel: String,
+)
