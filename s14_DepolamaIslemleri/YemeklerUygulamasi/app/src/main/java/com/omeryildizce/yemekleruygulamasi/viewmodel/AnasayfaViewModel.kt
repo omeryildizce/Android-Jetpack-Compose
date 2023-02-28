cd093bf960a75df6d3bee568bcac0d751874ac04
omeryildizce <omeryildizce@gmail.com>
2023-02-28 22:58:01 +0300

    Update AnasayfaViewModel.kt

--- s14_DepolamaIslemleri/YemeklerUygulamasi/app/src/main/java/com/omeryildizce/yemekleruygulamasi/viewmodel/AnasayfaViewModel.kt
+++ s14_DepolamaIslemleri/YemeklerUygulamasi/app/src/main/java/com/omeryildizce/yemekleruygulamasi/viewmodel/AnasayfaViewModel.kt
@@ -1,13 +1,15 @@
 package com.omeryildizce.yemekleruygulamasi.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.omeryildizce.yemekleruygulamasi.entity.Yemekler
 import com.omeryildizce.yemekleruygulamasi.repo.YemeklerDaoRepository
 
-class AnasayfaViewModel: ViewModel(){
+class AnasayfaViewModel(application: Application): AndroidViewModel(application){
     var yemeklerListesi = MutableLiveData<List<Yemekler>>()
-    var yrepo = YemeklerDaoRepository()
+    var yrepo = YemeklerDaoRepository(application)
     init {
         yemekleriYukle()
         yemeklerListesi = yrepo.yemekleriGetir()
