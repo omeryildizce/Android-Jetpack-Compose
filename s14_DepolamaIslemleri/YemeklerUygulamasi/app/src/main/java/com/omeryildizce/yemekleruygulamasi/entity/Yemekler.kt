5554a1b6ec5c982c4a9db45caebec1c05a9d99a6
omeryildizce <omeryildizce@gmail.com>
2023-02-28 22:57:49 +0300

    Update Yemekler.kt

--- s14_DepolamaIslemleri/YemeklerUygulamasi/app/src/main/java/com/omeryildizce/yemekleruygulamasi/entity/Yemekler.kt
+++ s14_DepolamaIslemleri/YemeklerUygulamasi/app/src/main/java/com/omeryildizce/yemekleruygulamasi/entity/Yemekler.kt
@@ -1,3 +1,15 @@
 package com.omeryildizce.yemekleruygulamasi.entity
 
-data class Yemekler(var yemekId: Int, var yemekAdi:String, var yemekResimAdi:String, var yemekFiyat:Int)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity("yemekler")
+data class Yemekler(
+    @PrimaryKey(true)
+    @ColumnInfo("yemek_id") @NotNull var yemek_id: Int,
+    @ColumnInfo("yemek_adi") @NotNull var yemek_adi:String,
+    @ColumnInfo("yemek_resim_adi") @NotNull var yemek_resim_adi:String,
+    @ColumnInfo("yemek_fiyat") @NotNull  var yemek_fiyat:Int
+    )
