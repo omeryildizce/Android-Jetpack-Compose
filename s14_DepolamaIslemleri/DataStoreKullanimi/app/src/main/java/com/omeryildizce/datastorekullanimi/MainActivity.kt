5006cd7fc475b936e3ca9efe21e748d84cf97851
omeryildizce <omeryildizce@gmail.com>
2023-02-28 15:11:01 +0300

    Update MainActivity.kt

--- s14_DepolamaIslemleri/DataStoreKullanimi/app/src/main/java/com/omeryildizce/datastorekullanimi/MainActivity.kt
+++ s14_DepolamaIslemleri/DataStoreKullanimi/app/src/main/java/com/omeryildizce/datastorekullanimi/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.omeryildizce.datastorekullanimi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.omeryildizce.datastorekullanimi.ui.theme.DataStoreKullanimiTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Sayfa()
                 }
             }
         }
@@ -30,14 +38,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Sayfa() {
+    val context = LocalContext.current
+    val ads = AppDataStore(context)
+
+    LaunchedEffect(key1 = true) {
+        val job: Job = CoroutineScope(Dispatchers.Main).launch {
+            // Kayıt
+            ads.kayitAd("Ömer")
+            ads.kayitYas(23)
+            ads.kayitBoy(1.95)
+            ads.kayitBekarMi(true)
+            val liste = HashSet<String>()
+            liste.add("Mehmet")
+            liste.add("Ali")
+            liste.add("Fatma")
+            liste.add("Betül")
+            ads.kayitArkadasListe(liste)
+
+            // Okuma
+            val gelenAd = ads.okuAd()
+            val gelenYas = ads.okuYas()
+            val gelenBoy = ads.okuBoy()
+            val gelenBekarMi = ads.okuBekarMi()
+            val gelenKayitArkadasListe = ads.okuArkadasListe()
+
+            Log.e("Bilgi", "Ad: $gelenAd")
+            Log.e("Bilgi", "Yas: $gelenYas")
+            Log.e("Bilgi", "Boy: $gelenBoy")
+            Log.e("Bilgi", "Bekar mı?: $gelenBekarMi")
+            Log.e("Bilgi", "Arkadaş Sayıs: ${gelenKayitArkadasListe.size}")
+            for ((index, arkadas) in gelenKayitArkadasListe.withIndex()) {
+                Log.e("Bilgi", "${index + 1}. $arkadas")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DataStoreKullanimiTheme {
-        Greeting("Android")
+        Sayfa()
     }
 }
