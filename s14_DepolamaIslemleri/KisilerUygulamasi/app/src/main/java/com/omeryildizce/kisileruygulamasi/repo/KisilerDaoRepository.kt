c06f792f8cfe12dabec6f63f26bfff39b68a1f5f
omeryildizce <omeryildizce@gmail.com>
2023-02-28 22:17:01 +0300

    Update KisilerDaoRepository.kt

--- s14_DepolamaIslemleri/KisilerUygulamasi/app/src/main/java/com/omeryildizce/kisileruygulamasi/repo/KisilerDaoRepository.kt
+++ s14_DepolamaIslemleri/KisilerUygulamasi/app/src/main/java/com/omeryildizce/kisileruygulamasi/repo/KisilerDaoRepository.kt
@@ -1,39 +1,61 @@
 package com.omeryildizce.kisileruygulamasi.repo
 
+import android.app.Application
+import android.content.Context
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.omeryildizce.kisileruygulamasi.entity.Kisiler
+import com.omeryildizce.kisileruygulamasi.room.Veritabani
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class KisilerDaoRepository {
+class KisilerDaoRepository(var application: Application) {
     var kisilerListesi = MutableLiveData<List<Kisiler>>()
+    var vt: Veritabani
 
     init {
+        vt = Veritabani.veritabaniErisim(application)!!
         kisilerListesi = MutableLiveData()
+
     }
-    fun kisileriGetir():MutableLiveData<List<Kisiler>>{
+
+    fun kisileriGetir(): MutableLiveData<List<Kisiler>> {
         return kisilerListesi
     }
+
     fun tumKisileriAl() {
-        val liste = mutableListOf<Kisiler>()
-        val k1 = Kisiler(1, "Ahmet", "0541111111")
-        val k2 = Kisiler(2, "Ömer", "0541111111")
-        liste.add(k1)
-        liste.add(k2)
-
-        kisilerListesi.value = liste
+        val job: Job = CoroutineScope(Dispatchers.Main).launch {
+            kisilerListesi.value = vt.kisilerDao().tumKisiler()
+        }
     }
 
-    fun kisiAra(aramaKelimesi:String){
+    fun kisiAra(aramaKelimesi: String) {
+        val job: Job = CoroutineScope(Dispatchers.Main).launch {
+            kisilerListesi.value = vt.kisilerDao().kisiArama(aramaKelimesi)
+        }
+    }
 
-        Log.e("Arama", "kisiAra: $aramaKelimesi")
+    fun kisiKayit(kisiAd: String, kisiTel: String) {
+        val job: Job = CoroutineScope(Dispatchers.Main).launch {
+            val kisi = Kisiler(0, kisiAd, kisiTel)
+            vt.kisilerDao().kisiEkle(kisi)
+        }
     }
-    fun kisiKayit(kisiAd:String, kisiTel:String){
-        Log.e("Kişi Kayıt", "$kisiAd - $kisiTel" )
+
+    fun kisiGuncelle(kisiId: Int, kisiAd: String, kisiTel: String) {
+        val job: Job = CoroutineScope(Dispatchers.Main).launch {
+            val kisi = Kisiler(kisiId, kisiAd, kisiTel)
+            vt.kisilerDao().kisiGuncelle(kisi)
+        }
     }
-    fun kisiGuncelle(kisiId: Int,kisiAd:String, kisiTel:String){
-        Log.e("Kişi Güncelle", "$kisiId:  $kisiAd - $kisiTel" )
-    }
-    fun kisiSilme(kisiId: Int ){
-        Log.e("Kişi Sil", "$kisiId" )
+
+    fun kisiSilme(kisiId: Int) {
+        val job: Job = CoroutineScope(Dispatchers.Main).launch {
+            val kisi = Kisiler(kisiId, "","")
+            vt.kisilerDao().kisiSil(kisi)
+            tumKisileriAl()
+        }
     }
 }
