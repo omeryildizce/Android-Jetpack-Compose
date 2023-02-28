e5ea4a9862c60d7c00269ac7ac125afb71b8e583
omeryildizce <omeryildizce@gmail.com>
2023-02-28 19:44:50 +0300

    Create Veritabani.kt

---
+++ s14_DepolamaIslemleri/RoomKullanimi/app/src/main/java/com/omeryildizce/roomkullanimi/Veritabani.kt
@@ -0,0 +1,29 @@
+package com.omeryildizce.roomkullanimi
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Kisiler::class], version = 1)
+abstract class Veritabani : RoomDatabase() {
+    abstract fun kisilerDao(): KisilerDao
+
+    companion object {
+        var INSTANCE: Veritabani? = null
+        fun veritabaniErisim(context: Context): Veritabani? {
+            if (INSTANCE == null) {
+                synchronized(Veritabani::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        Veritabani::class.java,
+                        "rehber.sqlite"
+                    )
+                        .createFromAsset("rehber.sqlite")
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+}
