cc2949b8955650f23bfe3213a82bb8cdf6d8ec8f
omeryildizce <omeryildizce@gmail.com>
2023-02-28 22:17:11 +0300

    Update KisiDetaySayfaViewModel.kt

--- s14_DepolamaIslemleri/KisilerUygulamasi/app/src/main/java/com/omeryildizce/kisileruygulamasi/viewmodel/KisiDetaySayfaViewModel.kt
+++ s14_DepolamaIslemleri/KisilerUygulamasi/app/src/main/java/com/omeryildizce/kisileruygulamasi/viewmodel/KisiDetaySayfaViewModel.kt
@@ -1,10 +1,12 @@
 package com.omeryildizce.kisileruygulamasi.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.omeryildizce.kisileruygulamasi.repo.KisilerDaoRepository
 
-class KisiDetaySayfaViewModel: ViewModel() {
-    var krepo = KisilerDaoRepository()
+class KisiDetaySayfaViewModel(application: Application) : AndroidViewModel(application) {
+    var krepo = KisilerDaoRepository(application)
     fun guncelle(kisiId:Int, kisiAd:String, kisiTel:String){
         krepo.kisiGuncelle(kisiId, kisiAd, kisiTel)
     }
