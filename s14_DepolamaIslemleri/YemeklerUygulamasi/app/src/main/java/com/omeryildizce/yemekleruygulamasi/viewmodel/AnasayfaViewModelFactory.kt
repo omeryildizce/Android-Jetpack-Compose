2e38b71d768591e0bde220dc4ee51ad48001d9a8
omeryildizce <omeryildizce@gmail.com>
2023-02-28 22:58:04 +0300

    Create AnasayfaViewModelFactory.kt

---
+++ s14_DepolamaIslemleri/YemeklerUygulamasi/app/src/main/java/com/omeryildizce/yemekleruygulamasi/viewmodel/AnasayfaViewModelFactory.kt
@@ -0,0 +1,11 @@
+package com.omeryildizce.yemekleruygulamasi.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class AnasayfaViewModelFactory(var application: Application):ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return AnasayfaViewModel(application) as T
+    }
+}
