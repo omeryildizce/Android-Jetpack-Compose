4d046148401b93792c219056185616f3386c96ce
omeryildizce <omeryildizce@gmail.com>
2023-02-28 22:17:13 +0300

    Update KisiKayitSayfaViewModel.kt

--- s14_DepolamaIslemleri/KisilerUygulamasi/app/src/main/java/com/omeryildizce/kisileruygulamasi/viewmodel/KisiKayitSayfaViewModel.kt
+++ s14_DepolamaIslemleri/KisilerUygulamasi/app/src/main/java/com/omeryildizce/kisileruygulamasi/viewmodel/KisiKayitSayfaViewModel.kt
@@ -1,10 +1,12 @@
 package com.omeryildizce.kisileruygulamasi.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.omeryildizce.kisileruygulamasi.repo.KisilerDaoRepository
 
-class KisiKayitSayfaViewModel: ViewModel() {
-    var krepo = KisilerDaoRepository()
+class KisiKayitSayfaViewModel(application: Application) : AndroidViewModel(application) {
+    var krepo = KisilerDaoRepository(application)
     fun kayit(kisiAd:String, kisiTel:String){
         krepo.kisiKayit(kisiAd, kisiTel)
     }
