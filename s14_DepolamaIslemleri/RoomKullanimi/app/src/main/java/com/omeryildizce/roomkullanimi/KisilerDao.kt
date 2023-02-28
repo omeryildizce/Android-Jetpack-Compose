39ae62a69be01c5db542fd72f4ba2150234770f8
omeryildizce <omeryildizce@gmail.com>
2023-02-28 20:40:51 +0300

    Update KisilerDao.kt

--- s14_DepolamaIslemleri/RoomKullanimi/app/src/main/java/com/omeryildizce/roomkullanimi/KisilerDao.kt
+++ s14_DepolamaIslemleri/RoomKullanimi/app/src/main/java/com/omeryildizce/roomkullanimi/KisilerDao.kt
@@ -1,9 +1,6 @@
 package com.omeryildizce.roomkullanimi
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface KisilerDao {
@@ -15,4 +12,19 @@ interface KisilerDao {
 
     @Update
     suspend fun kisiGuncelle(kisiler: Kisiler)
+
+    @Delete
+    suspend fun kisiSil(kisiler:Kisiler)
+
+    @Query("Select * from kisiler order by random() limit 1")
+    suspend fun rastgeleKisiGetir():List<Kisiler>
+
+    @Query("Select * from kisiler where kisi_ad like '%'|| :aramaKelimesi || '%'")
+    suspend fun kisiAra(aramaKelimesi:String):List<Kisiler>
+
+    @Query("Select * from kisiler where kisi_id = :kisi_id")
+    suspend fun kisiGetir(kisi_id:Int):Kisiler
+
+    @Query("select count(*) from kisiler where kisi_ad = :kisi_ad")
+    suspend fun kayitKontrol(kisi_ad: String):Int
 }
