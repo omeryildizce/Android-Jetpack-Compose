7613d5802ad2f8c6ecfabd7e95bf79b4cff650c8
omeryildizce <omeryildizce@gmail.com>
2023-02-28 19:44:48 +0300

    Update MainActivity.kt

--- s14_DepolamaIslemleri/RoomKullanimi/app/src/main/java/com/omeryildizce/roomkullanimi/MainActivity.kt
+++ s14_DepolamaIslemleri/RoomKullanimi/app/src/main/java/com/omeryildizce/roomkullanimi/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.omeryildizce.roomkullanimi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,9 +9,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.omeryildizce.roomkullanimi.ui.theme.RoomKullanimiTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Sayfa()
                 }
             }
         }
@@ -30,14 +37,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Sayfa() {
+    val  context = LocalContext.current
+    val vt = Veritabani.veritabaniErisim(context)
+    LaunchedEffect(key1 = true){
+        if (vt != null) {
+          //  ekle(vt)
+            tumKisiler(vt)
+        }
+    }
 }
-
+fun tumKisiler(vt:Veritabani){
+    val job:Job = CoroutineScope(Dispatchers.Main).launch {
+        val liste = vt.kisilerDao().tumKisiler()
+        for (k in liste ){
+            Log.e( "Kişi: ", "**********" )
+            Log.e("Kişi Id", "${k.kisi_id}")
+            Log.e("Kişi Ad", "${k.kisi_adi}")
+            Log.e("Kişi Telefon", "${k.kisi_tel}")
+        }
+    }
+}
+fun ekle(vt: Veritabani){
+    val job : Job = CoroutineScope(Dispatchers.Main).launch {
+        val yeniKisi = Kisiler(0, "Mehmet", "3333")
+        vt.kisilerDao().kisiEkle(yeniKisi)
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RoomKullanimiTheme {
-        Greeting("Android")
+        Sayfa()
     }
 }
